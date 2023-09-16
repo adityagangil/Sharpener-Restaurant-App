96d203aeafb09ff9df6c53e4ec81d97038bb1ad9
[aditya gangil] <[adityagangil2001@gmail.com]>
2023-09-16 11:03:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Fragment } from "react";
-import Header from "./components/Layout/Header";
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
-};
+}
 
 export default App;
